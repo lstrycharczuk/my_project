af3358f14fdfabdda8f73693d0276dcf286e65c3
Laura Strycharczuk <u21_laustr_lbn@technischools.com>
2024-11-26 12:27:44 +0000

    app update

--- src/my_project_frontend/src/App.vue
+++ src/my_project_frontend/src/App.vue
@@ -1,17 +1,27 @@
 <script setup>
 import { ref } from 'vue';
 import { my_project_backend } from 'declarations/my_project_backend/index';
-let greeting = ref('');
+let chat = ref('');
+
+async function getChats() {
+  chat.value = await my_project_backend.get_chat()
+}
 
 async function handleSubmit(e) {
   e.preventDefault();
   const target = e.target;
-  const chat = target.querySelector('#chat').value;
-  await my_project_backend.save_chat(chat).then((response) => {
-    greeting.value = response;
-  });
+  const msg = target.querySelector('#chat').value;
+  await my_project_backend.save_chat(msg)
+  await getChats()
 }
+
+
+
+getChats()
+
 </script>
+
+
 
 <template>
   <main>
@@ -20,9 +30,11 @@ async function handleSubmit(e) {
     <br />
     <form action="#" @submit="handleSubmit">
       <label for="chat">Enter your msg: &nbsp;</label>
-      <input id="chat" alt="Name" type="text" />
+      <input id="chat" alt="Chat" type="text" />
       <button type="submit">Click Me!</button>
     </form>
-    <section id="greeting">{{ greeting }}</section>
+    <section id="greeting">
+      <div v-for="msg in chat">{{ msg }}</div>
+    </section>
   </main>
 </template>
